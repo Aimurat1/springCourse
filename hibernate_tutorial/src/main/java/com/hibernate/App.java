ed243af14bd56b3ccc607456ab4d1000b75d7511
Aimurat <aimurat927@gmail.com>
2023-01-04 22:51:43 +0500

    Hibernate Update and Delete

--- hibernate_tutorial/src/main/java/com/hibernate/App.java
+++ hibernate_tutorial/src/main/java/com/hibernate/App.java
@@ -25,15 +25,30 @@ public class App {
             // System.out.println(person.getName() + ' ' + person.getAge());
 
             // Insert Person
-            Person person1 = new Person("Tom", 22);
-            Person person2 = new Person("Bob", 45);
-            Person person3 = new Person("Michael", 54);
+            // Person person1 = new Person("Tom", 22);
+            // Person person2 = new Person("Bob", 45);
+            // Person person3 = new Person("Michael", 54);
 
-            session.save(person1);
-            session.save(person2);
-            session.save(person3);
+            // session.save(person1);
+            // session.save(person2);
+            // session.save(person3);
+
+            // Update Person
+            // Person person = session.get(Person.class, 1);
+            // person.setName("Changed");
+
+            // Delete Person
+            // Person person1 = session.get(Person.class, 2);
+            // session.delete(person1);
+
+            // Get ID of Person
+            Person person = new Person("Some name", 11);
+            session.save(person);
 
             session.getTransaction().commit();
+
+            // Get ID of Person
+            System.out.println(person.getId());
         } finally {
             sessionFactory.close();
         }
